cdb5420355f315d322db8f87cb7a71c6f5d1c210
yarabande <moreira.bande.y@gmail.com>
2023-08-28 14:26:45 +0200

    a

--- resources/views/layout/header_footer.blade.php
+++ resources/views/layout/header_footer.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="x-ua-compatible" content="ie=edge" />
-	<title>Finanix - Consulting & Business HTML5 Template</title>
+	<title>Escopil Tecnologia</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 	<link rel="manifest" href="site.webmanifest" />
